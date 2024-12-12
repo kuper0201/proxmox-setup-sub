cf5ca358c3f16e506bba3444df63385f29e0d985
kuper0201 <kuper171@gmail.com>
2024-12-12 16:49:30 +0900

    Update auto_off.sh

--- auto_off.sh
+++ auto_off.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-running_qm=$(qm list | grep "running")
-running_pct=$(pct list | grep "running")
+running_qm=$(/usr/sbin/qm list | grep "running")
+running_pct=$(/usr/sbin/pct list | grep "running")
 
 if [ -z "$running_qm" ] && [ -z "$running_pct" ]; then
     poweroff
